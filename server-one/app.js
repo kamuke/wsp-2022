0edfbabc82cda8fbc7b287b606349d06956d1c47
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-10-27 15:10:11 +0300

    server-one: changed weight on app.js

--- server-one/app.js
+++ server-one/app.js
@@ -10,7 +10,7 @@ app.get('/catinfo', (req, res) => {
     const cat = {
         name: 'Frank',
         birthdate: '2010-12-25',
-        weight: 5,
+        weight: 8,
     };
     res.json(cat);
 });
