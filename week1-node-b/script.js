77435a2113a8f1d91487a469c80e415912d73fe4
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-10-27 11:41:21 +0300

    week1-node and server-one

---
+++ week1-node-b/script.js
@@ -0,0 +1,7 @@
+const lodash = require('lodash');
+
+const a = 'Foo Bar';
+const b = lodash.camelCase(a);
+
+console.log('This is JavaScript.');
+console.log(b);
--- week1-node/script.js
+++
@@ -1,5 +0,0 @@
-const _ = require('lodash');
-let string = _.camelCase('Foo Bar');
-
-console.log('This is Node.js');
-console.log(string);
