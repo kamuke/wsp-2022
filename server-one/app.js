8deae8e6600d114c1cd70555296394fdb9207a95
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-10-27 19:08:03 +0300

    pug exercise, rendering index.html with pug

--- server-one/app.js
+++ server-one/app.js
@@ -6,20 +6,20 @@ const port = 3000;
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
-});
-
-// app.use(express.static('public'));
-
-// app.get('/catinfo', (req, res) => {
-//     const cat = {
-//         name: 'Frank',
-//         birthdate: '2010-12-25',
-//         weight: 8,
-//     };
-//     res.json(cat);
+// app.get('/', (req, res) => {
+//     res.render('index', { title: 'Hey', message: 'Hello there!' })
 // });
+
+app.use(express.static('public'));
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+        name: 'Frank',
+        birthdate: '2010-12-25',
+        weight: 8,
+    };
+    res.json(cat);
+});
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
