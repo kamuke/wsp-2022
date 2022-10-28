d232afbc7f156fda86982c4282d35fe946ca5042
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-10-28 16:49:47 +0300

    server two pug exercise

--- server-one/app.js
+++ server-one/app.js
@@ -5,12 +5,17 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
-
-// app.get('/', (req, res) => {
-//     res.render('index', { title: 'Hey', message: 'Hello there!' })
-// });
+app.set('views', './views');
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/kokeilu', (req, res) => {
+    res.render('test');
+});
 
 app.get('/catinfo', (req, res) => {
     const cat = {
@@ -20,10 +25,6 @@ app.get('/catinfo', (req, res) => {
     };
     res.json(cat);
 });
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
