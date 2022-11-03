9e74c5d6a0cfda2cdaffb48295466b8862442627
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-11-03 11:08:03 +0200

    getcats from database

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -6,13 +6,18 @@ const cat_list_get = async (req, res) => {
   res.json(await getAllCats());
 };
 
-const cat_get = (req, res) => {
-    const cat = getCat(req.params.id);
-    res.json(cat);
+const cat_get = async (req, res) => {
+  const cat = await getCat(req.params.id);
+
+  if (cat.length > 0) {
+    res.json(cat.pop());
+  } else {
+    res.send('Virhe');
+  }
 };
 
 const cat_post = (req, res) => {
-  console.log('cat_post', req.body, 'cat_post', req.file);
+  // console.log('cat_post', req.body, 'cat_post', req.file);
   res.send('Cat post done.');
 };
 
--- week2/models/catModel.js
+++ week2/models/catModel.js
@@ -5,14 +5,29 @@ const promisePool = pool.promise();
 
 const getAllCats = async () => {
   try {
-    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
-    const [rows] = await promisePool.query('SELECT * FROM wsp_cat');
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wsp_user table).
+    const [rows] = await promisePool.execute(`SELECT cat_id, wsp_cat.name, weight, owner, filename, birthdate, wsp_user.name AS ownername
+                                              FROM wsp_cat INNER JOIN wsp_user 
+                                              ON wsp_cat.owner = wsp_user.user_id`);
     return rows;
   } catch (e) {
     console.error('error', e.message);
   }
 };
 
+const getCat = async (catId) => {
+  try {
+    const [rows] = await promisePool.execute(`SELECT cat_id, wsp_cat.name, weight, owner, filename, birthdate, wsp_user.name AS ownername
+                                              FROM wsp_cat INNER JOIN wsp_user 
+                                              ON wsp_cat.owner = wsp_user.user_id
+                                              WHERE cat_id = ?;`, [catId]);
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
+}
+
 module.exports = {
   getAllCats,
+  getCat,
 };
