cb8047d21f39b5f3f4fd6e99346716d9065884e2
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-11-03 10:31:04 +0200

    database started

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -1,17 +1,13 @@
 // catController
 'use strict';
-const {cats, getCat} = require('../models/catModel');
+const {getAllCats, getCat} = require('../models/catModel');
 
-// const catModel = require('../models/catModel');
-// const cats = catModel.cats;
-
-const cat_list_get = (req, res) => {
-  res.json(cats);
+const cat_list_get = async (req, res) => {
+  res.json(await getAllCats());
 };
 
 const cat_get = (req, res) => {
     const cat = getCat(req.params.id);
-    // console.log('kissa', cat);
     res.json(cat);
 };
 
--- week2/models/catModel.js
+++ week2/models/catModel.js
@@ -1,37 +1,18 @@
+// ./models/catModel.js
 'use strict';
-const cats = [
-  {
-    id: '1',
-    name: 'Frank',
-    birthdate: '2010-10-30',
-    weight: '5',
-    owner: '1',
-    filename: 'http://placekitten.com/400/300',
-  },
-  {
-    id: '2',
-    name: 'James',
-    birthdate: '2015-12-25',
-    weight: '11',
-    owner: '2',
-    filename: 'http://placekitten.com/400/302',
-  },
-];
+const pool = require('../database/db');
+const promisePool = pool.promise();
 
-const getCat = (catId) => {
-  return cats.find((cat) => {
-    if (catId === cat.id) {
-      return cat;
-    }
-  });
-
-  // Shorter version
-  // return cats.find(cat => catId === cat.id);
+const getAllCats = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const [rows] = await promisePool.query('SELECT * FROM wsp_cat');
+    return rows;
+  } catch (e) {
+    console.error('error', e.message);
+  }
 };
 
-// const getCat = catId => cats.find(cat => catId === cat.id);
-
 module.exports = {
-  cats,
-  getCat,
+  getAllCats,
 };
