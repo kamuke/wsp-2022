eba157112417061425ea26d146e93981b2b0b692
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-11-11 22:40:46 +0200

    catRoute filefilter

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -1,14 +1,34 @@
 'use strict';
 const express = require('express');
 const {body} = require('express-validator');
-const multer  = require('multer')
-const upload = multer({ dest: './uploads/' })
+const multer = require('multer');
 const {cat_list_get, cat_get, cat_post, cat_put, cat_delete} = require('../controllers/catController');
 const router = express.Router();
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || 
+        file.mimetype === 'image/png' || 
+        file.mimetype === 'image/gif') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const testFile = (req, res, next) => {
+    if (req.file) {
+        next();
+    } else {
+        res.status(400).json({errors: 'File is not image'});
+    }
+};
+
+const upload = multer({ dest: './uploads/', fileFilter });
 
 router.route('/').
     get(cat_list_get).
     post(upload.single('cat'),
+        testFile,
         body('name').isLength({min: 1}).escape(),
         body('birthdate').isDate(),
         body('weight').isNumeric(),
