81df1d64513aa76bcacd955718f185d90158b1a8
Kerttu <80481715+kamuke@users.noreply.github.com>
2022-11-24 11:33:58 +0200

    https

--- week2/app.js
+++ week2/app.js
@@ -1,4 +1,5 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const authRoute = require('./routes/authRoute');
@@ -9,8 +10,14 @@ const passport = require('./utils/pass');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+    require('./utils/production')(app, process.env.HTTP_PORT || 3000, process.env.HTTPS_PORT || 8000);
+} else {
+    require('./utils/localhost')(app, process.env.PORT || 3000);
+}
 
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
@@ -32,5 +39,3 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500).
         json({message: err.message || 'Internal server error'});
 });
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
